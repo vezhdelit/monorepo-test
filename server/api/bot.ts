499715bfe4174357b420c76122fca45ce066efe5
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-05-10 15:48:13 +0300

    feat: added vercel config

--- server/api/bot.ts
+++ server/api/bot.ts
@@ -1,4 +1,7 @@
-import { Bot } from "grammy";
+import { Bot, webhookCallback } from "grammy";
+
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error("BOT_TOKEN is unset");
 
 const bot = new Bot("7198756818:AAGIRl5svb0Yl8zxZSZIfAY8f408t0VIL2o");
 
@@ -7,3 +10,5 @@ bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
 
 bot.start();
+
+export default webhookCallback(bot, "std/http");
--- server/bot.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const grammy_1 = require("grammy");
-const bot = new grammy_1.Bot("7198756818:AAGIRl5svb0Yl8zxZSZIfAY8f408t0VIL2o");
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
-bot.start();
